d11a265eb5084c0f2e096fcbdc8ea2a7d7be4570
CLEMENTE CAMPOS <clemente.campos@uc.cl>
2024-10-22 20:13:33 -0300

    avance clase Manager

--- grafo/main.py
+++ grafo/main.py
@@ -1,18 +1,25 @@
-# import env
-from clases.clases import Ship, Port, Route
-import simpy
+# # import env
+# from clases.clases import Ship, Port, Route
+# import simpy
 
-env = simpy.Environment()
-puerto_1 = Port(env, "san antonio", 100)
-puerto_2 = Port(env, "valparaiso", 100)
-puerto_3 = Port(env, "antofagasta", 100)
-ruta_1 = Route(1, puerto_1, puerto_2, 10)
-ruta_2 = Route(2, puerto_2, puerto_3, 15)
-ruta_3 = Route(3, puerto_3, puerto_1, 8)
-ship_1 = Ship(env, "barco1", 1, puerto_1)
-ship_2 = Ship(env, "barco2", 1, puerto_2)
-ship_3 = Ship(env, "barco3", 1, puerto_3)
-env.process(ship_1.drive(ruta_1))
-env.process(ship_2.drive(ruta_2))
-env.process(ship_3.drive(ruta_3))
-env.run(until=15)
+# env = simpy.Environment()
+# puerto_1 = Port(env, "san antonio", 100)
+# puerto_2 = Port(env, "valparaiso", 100)
+# puerto_3 = Port(env, "antofagasta", 100)
+# ruta_1 = Route(1, puerto_1, puerto_2, 10)
+# ruta_2 = Route(2, puerto_2, puerto_3, 15)
+# ruta_3 = Route(3, puerto_3, puerto_1, 8)
+# ship_1 = Ship(env, "barco1", 1, puerto_1)
+# ship_2 = Ship(env, "barco2", 1, puerto_2)
+# ship_3 = Ship(env, "barco3", 1, puerto_3)
+# env.process(ship_1.drive(ruta_1))
+# env.process(ship_2.drive(ruta_2))
+# env.process(ship_3.drive(ruta_3))
+# env.run(until=15)
+
+from clases.clases import Manager
+
+manager = Manager()
+manager.add("ports.txt", "routes.txt", "ships.txt")
+manager.processes([0, 1, 2])
+manager.run(15)
