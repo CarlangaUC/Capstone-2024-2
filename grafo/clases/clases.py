374a7cc3dd03792855e636e380501c9cefa67601
CLEMENTE CAMPOS <clemente.campos@uc.cl>
2024-11-25 20:58:26 -0300

    recursos rutas + PEP8

--- grafo/clases/clases.py
+++ grafo/clases/clases.py
@@ -9,8 +9,8 @@ UNIT_TIME = 1
 class Ship:
     ship_id = 0
 
-    def __init__(self, env, name, speed, port_id, cycles,recharge,itinerary):
-        
+    def __init__(self, env, name, speed, port_id,
+                 cycles, recharge, itinerary):
         self.env = env
         self.name = name
         self.speed = speed
@@ -22,45 +22,47 @@ class Ship:
         self.cycles = bool(cycles)
         self.route_id = ""
         Ship.ship_id += 1
-        #Tiempo de carga y descarga y metemos el itinerario de barcos como interno en simpy
-        self.recharge = recharge 
+        # Tiempo de carga y descarga y metemos el itinerario
+        # de barcos como interno en simpy
+        self.recharge = recharge
         self.itinerary = itinerary
         self.actual_port = port_id
-        
-    def unload(self,archivo):
+
+    def unload(self, archivo):
         # simula la descarga del barco, espera según la carga que tiene
-        #archivo.write(f"Barco {self.ship_id} descargando...\n")
-        archivo.write(f"event;ES2;{self.ship_id};{self.actual_port};{self.env.now}\n")
+        archivo.write(f"event;ES2;{self.ship_id};{self.actual_port};"
+                      f"{self.env.now}\n")
         print(f"Barco {self.ship_id} descargando...")
         yield self.env.timeout(self.recharge)
 
-    def drive(self, final_port, dist, route_id,archivo,matriz_adyacencia): #Metodo importante, pensar sobrecarga de puertos y cambio de rutas
-        
-        
-        #Recurso compartido, no es necesario preguntarse si cierra/capacidad llena a mitad del viaje dado que lo parte estando lleno
- 
-        with final_port.resource.request() as request: # ENCALLADO PERO ESTA ABIERTO EL PUERTO PENSAR
+# Método importante, pensar sobrecarga de puertos y cambio de rutas
+    def drive(self, final_port, route, archivo, matriz_adyacencia):
+        # Recurso compartido, no es necesario preguntarse si cierra/capacidad
+        # llena a mitad del viaje dado que lo parte estando lleno
+        with route.resource.request() as request:
+            print(f"{self.name} esperando...")
             yield request
-            
-            while self.pos < dist:
-                #archivo.write(f"{self.name}, ruta {route_id}, posicion: {self.pos}, "
-                    #f"tiempo simulacion {self.env.now}\n")
+            while self.pos < route.dist:
                 self.pos += self.speed
-                pos_total = round(self.pos/dist,2)
-                #Escribir output formato
-                archivo.write(f"event;ES1;{self.ship_id};{self.actual_port}-{final_port.port_id};{pos_total};{self.env.now}\n")
-                print(f"{self.name}, ruta {route_id}, posicion: {self.pos}, "
-                    f"tiempo simulacion {self.env.now}")
+                pos_total = round(self.pos/route.dist, 2)
+                # Escribir output formato
+                archivo.write(f"event;ES1;{self.ship_id};{self.actual_port}-"
+                              f"{final_port.port_id};{pos_total};"
+                              f"{self.env.now}\n")
+                print(f"{self.name}, ruta {route.route_id}, "
+                      f"posicion: {self.pos}, "
+                      f"tiempo simulacion {self.env.now}")
                 yield self.env.timeout(UNIT_TIME)
+        with final_port.resource.request() as request:
+            yield request
             self.pos = 0
             final_port.ships.append(self.ship_id)
             # al hacer yield del proceso esperamos a que
             # la función unload termine
-            
-            #MATAR Y VERIFICAR SI ESTA ABIERTO NUEVAMENTE
-            
+            # MATAR Y VERIFICAR SI ESTA ABIERTO NUEVAMENTE
             yield self.env.process(self.unload(archivo))
             final_port.ships.remove(self.ship_id)
+
 
 class Port:
 
@@ -70,13 +72,13 @@ class Port:
         self.capacity = capacity
         self.port_id = port_id
         self.ships = []
-        self.open = True # ASUMIR QUE TODOS PARTEN ABIERTOS
+        self.open = True  # ASUMIR QUE TODOS PARTEN ABIERTOS
         self.resource = simpy.Resource(env, capacity=capacity)
 
 
 class Route:
 
-    def __init__(self, env, initial_port_id, final_port_id, dist):
+    def __init__(self, env, initial_port_id, final_port_id, dist, capacity):
         self.env = env
         self.initial_port_id = initial_port_id
         self.final_port_id = final_port_id
@@ -85,8 +87,10 @@ class Route:
         # búsqueda de una ruta más rápida
         self.route_id = f"{initial_port_id}-{final_port_id}"
         self.ships = []
+        self.resource = simpy.Resource(env, capacity=capacity)
+        self.open = True
+        # FALTA LA CAPACIDAD DE RUTA PERO COMENTAR Y PENSAR
 
-        #FALTA LA CAPACIDAD DE RUTA PERO COMENTAR Y PENSAR
 
 class Manager:
 
@@ -95,106 +99,86 @@ class Manager:
         self.ships = {}
         self.ports = {}
         self.routes = {}
-        self.archivo = open("archivo.txt","w")
-            
-    # representa el event loop para un barco en particular 
-    
+        self.archivo = open("archivo.txt", "w")
+    # representa el event loop para un barco en particular
+
     def search_route(self, actual_port_id, final_port, matriz_adyacencia):
-        
-        N = len(matriz_adyacencia)  
-        costos = [float('inf')] * N  
-        costos[actual_port_id] = 0  
-        previos = [-1] * N  
-        visitados = [False] * N  
-        
-        cola_prioridad = [(0, actual_port_id)]  
-        
+        N = len(matriz_adyacencia)
+        costos = [float('inf')] * N
+        costos[actual_port_id] = 0
+        previos = [-1] * N
+        visitados = [False] * N
+        cola_prioridad = [(0, actual_port_id)]
+
         while cola_prioridad:
             costo_actual, puerto_actual = heapq.heappop(cola_prioridad)
-            
             if visitados[puerto_actual]:
                 continue
-            
             visitados[puerto_actual] = True
-            
             if puerto_actual == final_port:
                 break
-            
             for vecino in range(N):
                 costo_ruta = matriz_adyacencia[puerto_actual][vecino]
-                
-                if costo_ruta > 0 and not visitados[vecino]:  
+                if costo_ruta > 0 and not visitados[vecino]:
                     nuevo_costo = costo_actual + costo_ruta
-                    
                     if nuevo_costo < costos[vecino]:
                         costos[vecino] = nuevo_costo
                         previos[vecino] = puerto_actual
                         heapq.heappush(cola_prioridad, (nuevo_costo, vecino))
-                        
         if costos[final_port] == float('inf'):
             print("No hay ruta disponible entre los puertos.")
-            return None  
-        
+            return None
         ruta = []
         puerto = final_port
         while puerto != -1 and previos[puerto] != -1:
             ruta.append(f"{previos[puerto]}-{puerto}")
             puerto = previos[puerto]
-        
-        ruta.reverse() 
-        
+        ruta.reverse()
         return ruta
-    
-    
-    def ship_event_loop(self, ship, events,archivo):
-        
+
+    def ship_event_loop(self, ship, events, archivo):
         actual_port_id = ship.port_id
         # si cicla, entonces cambiamos events por cycle(events),
         # que nos entrega una generador que repite los elementos
         # de events infinitamente
         events = ship.itinerary
-        
         test = events
-        
         if ship.cycles:
             events = cycle(events)
-        
-        #While para obligar cumplir itinerario
-        
+
+        # while para obligar cumplir itinerario
         visitados = set()
         while len(visitados) != len(test):
-            
-            for final_port_id in events: # Todos los puertos del itinerario 
-                
+            # todos los puertos del itinerario
+            for final_port_id in events:
                 if final_port_id not in visitados:
-                    
                     final_port = self.ports[final_port_id]
-                    
-                    if final_port.open == False: # Pensar logica de apertura de puertos indirectamente
+                    # pensar logica de apertura de puertos indirectamente
+                    if not final_port.open:
                         continue
-                    
-                    rutas = self.search_route(actual_port_id,final_port_id,self.matrix) # Ruta no directa necesariamente
-                    if rutas == None:
+                    rutas = self.search_route(actual_port_id,
+                                              final_port_id,
+                                              self.matrix)
+                    if rutas is None:
                         continue
-                    
-                    for ruta in rutas: 
-                        
-                        route = self.routes[ruta] 
+                    for ruta in rutas:
+                        route = self.routes[ruta]
                         route.ships.append(ship.ship_id)
                         # al hacer yield del proceso esperamos a que
                         # la función drive termine
-                        yield self.env.process(ship.drive(final_port,route.dist, route.route_id,archivo,self.matrix))
+                        yield self.env.process(ship.drive(final_port,
+                                                          route, archivo,
+                                                          self.matrix))
                         route.ships.remove(ship.ship_id)
                         actual_port_id = final_port_id
                         ship.actual_port = final_port_id
-                        
                     visitados.add(final_port_id)
 
     def processes(self):
-        #Procesar cada barco con su itinerario asociado
+        # procesar cada barco con su itinerario asociado
         for ship_id, ship in self.ships.items():
-            self.env.process(self.ship_event_loop(ship,ship.itinerary,self.archivo)) 
-            
+            self.env.process(self.ship_event_loop(ship, ship.itinerary,
+                                                  self.archivo))
 
     def run(self, until):
         self.env.run(until=until)
@@ -215,21 +199,22 @@ class Manager:
             file.readline()
             for line in file:
                 data = line.strip().split(";")
-                yield Route(self.env, int(data[0]),
-                            int(data[1]), int(data[2]))
+                yield Route(self.env, int(data[0]), int(data[1]),
+                            int(data[2]), int(data[3]))
 
     def ships_generator(self, ships_file):
         with open(ships_file) as file:
-            file.readline()  
+            file.readline()
             for line in file:
                 data = line.strip().split(";")
                 name = data[0]
                 speed = float(data[1])
                 port_id = int(data[2])
                 cycles = int(data[3])
-                recharge = int(data[4])  
-                itinerary = list(map(int, data[5].split(",")))  
-                yield Ship(self.env, name, speed, port_id, cycles, recharge, itinerary)
+                recharge = int(data[4])
+                itinerary = list(map(int, data[5].split(",")))
+                yield Ship(self.env, name, speed, port_id,
+                           cycles, recharge, itinerary)
 
     def add_ports(self, ports_file):
         for i, port in enumerate(self.ports_generator(ports_file)):
@@ -241,7 +226,7 @@ class Manager:
             self.routes[route.route_id] = route
             self.matrix[route.initial_port_id][route.final_port_id] = route.dist
         print(self.matrix)
-        
+
     def add_ships(self, ships_file):
         for ship in self.ships_generator(ships_file):
             self.ships[ship.ship_id] = ship
@@ -252,19 +237,13 @@ class Manager:
         self.add_ships(ships_file)
         self.output(self.archivo)
 
-    #Formar formato pedido
-    def output(self,archivo):
-        
+    # formar formato pedido
+    def output(self, archivo):
         for ship in self.ships.values():
-            archivo.write(f"ship;{ship.ship_id};{ship.name};{ship.itinerary[0]};{ship.speed};0.0\n")
-            
+            archivo.write(f"ship;{ship.ship_id};{ship.name};"
+                          f"{ship.itinerary[0]};{ship.speed};0.0\n")
         for port in self.ports.values():
-            
             archivo.write(f"port;{port.port_id};{port.name}\n")
-            
-        for rout in self.routes.values():
-            
-            archivo.write(f"route;{rout.initial_port_id}-{rout.final_port_id};1\n")
-            
-        
-#este evento es el de recorrer, lo metere en el input a cada uno, adapta la parte de leer barcos con esto en mente
+        for route in self.routes.values():
+            archivo.write(f"route;{route.initial_port_id}-"
+                          f"{route.final_port_id};1\n")
--- grafo/main.py
+++ grafo/main.py
@@ -1,16 +1,14 @@
-from collections import namedtuple
 from clases.clases import Manager
-#Event = namedtuple("Event", ["final_port_id", "load"])
 
-def manejar_archivo(archivo): # COMPLETAR (PROBABLEMENTE)
+
+def manejar_archivo(archivo):
     with open(archivo, "r") as datos_entrada:
         eventos = datos_entrada.strip().split("\n")
-    
     eventos_por_tiempo = {}
 
     for evento in eventos:
         partes = evento.split(";")
-        tiempo = int(partes[-1]) 
+        tiempo = int(partes[-1])
         if tiempo not in eventos_por_tiempo:
             eventos_por_tiempo[tiempo] = []
         eventos_por_tiempo[tiempo].append(evento)
@@ -20,17 +18,16 @@ def manejar_archivo(archivo): # COMPLETAR (PROBABLEMENTE)
         output.append(f"t={tiempo}")
         for evento in eventos_por_tiempo[tiempo]:
             partes = evento.split(";")
-            nuevo_evento = f"event;{partes[1]};{partes[2]};{partes[3]};{partes[4]}"
+            nuevo_evento = (f"event;{partes[1]};{partes[2]};"
+                            f"{partes[3]};{partes[4]}")
             output.append(nuevo_evento)
-    
+
     with open("output.txt", "w") as salida:
         for linea in output:
             salida.write(linea + "\n")
-            
+
+
 manager = Manager()
 manager.add("ports.txt", "routes.txt", "ships.txt")
-manager.processes() #Adaptado para que no sean eventos como tal 
-manager.run(50)
-
-
-
+manager.processes()
+manager.run(200)
