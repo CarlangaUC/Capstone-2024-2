d0e06cd3adeb195529bd4994323d1b339210a6e4
basticc <bastiancortescc@gmail.com>
2024-12-24 00:18:51 -0300

    rm-coments

--- grafo/main.py
+++ grafo/main.py
@@ -6,10 +6,6 @@ manager = Manager()
 
 ############### Input automatico ###########
 
-""" Observaciones:
-    - No funciona calculate metrics en este caso
-"""
-
 n_ports = 2
 manager.add(n_ports=n_ports)
 
@@ -18,5 +14,3 @@ t_simulacion = 2000
 manager.processes()
 manager.step_run(t_simulacion,sleep_time=0)
 
-
-# RUTAS NO TIENE EL ATRIBUTO DE CAPACITY ??? PORQUEE
